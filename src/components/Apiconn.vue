5b897b9ecd1bb0007bdfb9385a0296441b7916e8
Mayank238 <mayank.prajapati@bacancytechnology.com>
2019-02-14 15:21:46 +0530

    api connection

---
+++ src/components/Apiconn.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<div v-for="currency in info" class="currency" >
+    {{ currency.description }}:
+    <span class="lighten">
+      <span v-html="currency.symbol"></span><pre>{{ currency }}</pre>
+      <span v-html="raw"></span>
+    </span>
+  </div>
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+	data(){
+     return{
+     	info: null,
+     	raw: '<button>ABC</button>'
+     }
+	},
+	mounted(){
+		axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then((response) => (this.info = response.data))
+      .catch((err)=>{this.info = err})
+	}
+
+
+
+
+}
+
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <app-api></app-api>
+  </div>
+</template>
+<script>
+  import API from './Apiconn.vue'
+  export default{
+    components: {
+      'app-api':API
+    }
+  }
+</script>
